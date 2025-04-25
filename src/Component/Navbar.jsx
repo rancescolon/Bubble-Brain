7b6e1f4eade4db2990ebc18f6709558053154c5e
rancescolon <109361303+rancescolon@users.noreply.github.com>
2025-04-25 19:33:29 -0400

    git hub issue

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -286,7 +286,7 @@ const NavBar = () => {
               backgroundColor: "#f5f5f5",
               cursor: "pointer",
               "&:hover": {
-                backgroundColor: "#e0e0e0",
+                backgroundColor: "#e0e0e0"
               },
             }}
           >
